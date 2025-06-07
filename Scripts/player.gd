474a3f2fad31241c50e46ab7471851f36ee073b4
Tweny02 <yanguomuchi@gmail.com>
2025-06-07 15:30:18 +0800

    second commit

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,8 +1,9 @@
 '''
-Godot4.4
-v0.0.2.20250606
-	# init
-	# first commit
+Godot4.4.1
+v0.0.3.20250607
+	# develop
+		- move and slide
+	# second commit
 
 
 '''
@@ -12,4 +13,22 @@ extends CharacterBody2D
 
 class_name Player
 
-@export var speed : float
+# ============================VAR============================
+@export var speed : float = 60
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# ==========================FUNC================================
+func _process(delta: float) :
+	var direction = Input.get_vector("p1_left","p1_right","p1_up","p1_down")
+	velocity = direction * speed
+	
+	if velocity.length() > 0:
+		animation_player.play("run")
+		
+	else:
+		animation_player.play("idle")
+	
+	move_and_slide()
+	
+	
